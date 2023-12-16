86a4668803a94638650ab8718a4ddc8d7d451c69
Marcus <88283829+PyMarcus@users.noreply.github.com>
2023-12-15 21:25:20 -0300

    Add files via upload

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,4 +2,6 @@
 plugins {
     id("com.android.application") version "8.1.0" apply false
     id("org.jetbrains.kotlin.android") version "1.8.0" apply false
+    // room
+    id("org.jetbrains.kotlin.kapt") version "1.9.0"
 }
